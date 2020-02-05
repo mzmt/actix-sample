ccc18eaeadb8acbf473854b5cafac86b97af3abd
mzmt <mrcrius1355@gmail.com>
2020-02-05 14:13:17 +0900

    initialize realworld

--- diesel_demo/target/debug/deps/diesel_demo-5ad67b515367997c.d
+++ diesel_demo/target/debug/deps/diesel_demo-5ad67b515367997c.d
@@ -1,5 +1,7 @@
-/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-5ad67b515367997c.rmeta: src/lib.rs
+/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-5ad67b515367997c.rmeta: src/lib.rs src/models.rs src/schema.rs
 
-/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-5ad67b515367997c.d: src/lib.rs
+/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-5ad67b515367997c.d: src/lib.rs src/models.rs src/schema.rs
 
 src/lib.rs:
+src/models.rs:
+src/schema.rs:
--- diesel_demo/target/debug/deps/diesel_demo-f5a06a89fa76c5e3.d
+++ diesel_demo/target/debug/deps/diesel_demo-f5a06a89fa76c5e3.d
@@ -1,5 +1,7 @@
-/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-f5a06a89fa76c5e3.rmeta: src/lib.rs
+/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-f5a06a89fa76c5e3.rmeta: src/lib.rs src/models.rs src/schema.rs
 
-/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-f5a06a89fa76c5e3.d: src/lib.rs
+/Users/ryohei/Desktop/rust/actix-sample/diesel_demo/target/debug/deps/diesel_demo-f5a06a89fa76c5e3.d: src/lib.rs src/models.rs src/schema.rs
 
 src/lib.rs:
+src/models.rs:
+src/schema.rs:
